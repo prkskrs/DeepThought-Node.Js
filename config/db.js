1ba268128aedfa45aaaaffb27865c28e0562444c
Prakash Singh <97762141+prkskrs@users.noreply.github.com>
2022-08-17 04:34:07 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -12,52 +12,7 @@ const db = client.db(dbName);
 const collection=db.collection("Event")
 if(!collection){
 db.createCollection("Event",
-{
-  validator: {
-  $jsonSchema: {
-     bsonType: "object",
-     required: [ "name" ],
-     properties: {
-        name: {
-           bsonType: "string",
-           description: "must be a string and is required"
-        },
-        tagline: {
-           bsonType: "string",
-           description: ""
-        },
-        schedule: {
-          bsonType:"timestamp"
-        },
-        description: {
-           bsonType: "string"
-        },
-        image:{
-          bsonType:"string",
-          description:""
-        },
-        moderator:{
-         enum: [ "admin","user" ],
-         description: "can only be one of the enum values and is required"
-        },
-        category:{
-         enum: [ "category1","category2"],
-         description: "can only be one of the enum values and is required"
-        },
-        sub_category:{
-         enum: [ "sub_category1","sub_category2"],
-         description: "can only be one of the enum values and is required"
-        },
-        rigor_rank:{
-          bsonType:"int"
-        },
-        attendees:{
-          bsonType:["objectId"]
-        }
-    }
-  }
-}
-})
+{})
 }
 
 else{
